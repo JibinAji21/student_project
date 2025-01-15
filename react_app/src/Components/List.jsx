4f95b4d109d5f8c73ce1c59f806416848a12e9b4
JibinAji21 <jibinaji26@gmail.com>
2025-01-15 13:46:43 +0530

    student project

--- react_app/src/Components/List.jsx
+++ react_app/src/Components/List.jsx
@@ -1,13 +1,33 @@
-import { useEffect,useState } from "react";
+import { useEffect,useState } from "react"
+import axios from 'axios'
+
 
 function List(){
-    const{data,setData} = useState([])
+    const [data,SetData] = useState([])
+    const [editing,setEditing]=useState(false)
+    const [editdata,setEditData]=useState(null)
+
     useEffect(()=>{
-        axios.get('').then((res)=>)
-    })
+        axios.get('http://127.0.0.1:8000/api/student/').then((res)=>{
+            console.log(res.data);
+            SetData(res.data)
+            
+        }).catch(error => console.log(error.message))
+    },[])
+    const Edit_dtls= (task)=>{
+        setEditing(true)
+        setEditData(task)
+    }
+    const updateDtls = (id,task) =>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/student/${id}/`,task).then(res=>{
+            SetData(data.map((prv)=>prv.id===id ? res.data : prv))
+        }).catch(error =>console.log(error.message))
+    }
     return(
+
         <div className="container">
-            <h1>Display Details</h1>
+            <h1>Student Details</h1>
             <table className="table">
                 <thead> 
                     <tr>
@@ -24,7 +44,6 @@ function List(){
                             <td>{value.name}</td>
                             <td>{value.age}</td>
                             <td>{value.email}</td>
-                            <td>{value.completed ? 'completed':'not'}</td>
                             <td><button className="btn btn-outline-info" onClick={()=>{Edit_dtls(value)}}>Edit</button></td>
                             <td><button className="btn btn-outline-danger" onClick={()=>{}}>Delete</button></td>
                         </tr>
@@ -34,8 +53,10 @@ function List(){
             {editing ? <EditForm curTask= {editdata} updatefun={updateDtls}/>:null}
         </div>
     )
+
 }
-const EditForm=({curTask})=>{
+
+const EditForm=({curTask,updataefun})=>{
     const[task,setTask]=useState(curTask)
     const handleChange = (e) =>{
         const {name,value} = e.target 
@@ -47,8 +68,10 @@ const EditForm=({curTask})=>{
     }
     return(
         <form onSubmit={handleSubmit}>
-            <input type="text" name="title" id="title" value={task.task} onChange={handleChange} />
-            <input type="text" name="description" id="description" value={task.description} onChange={handleChange}/>
+            <input type="text" name="roll_no" id="roll_no" value={task.roll_no} onChange={handleChange} />
+            <input type="text" name="name" id="name" value={task.name} onChange={handleChange}/>
+            <input type="text" name="age" id="age" value={task.age} onChange={handleChange}/>
+            <input type="text" name="email" id="email" value={task.email} onChange={handleChange}/>
             <input type="submit"  value="Update"/>
 
         </form>
